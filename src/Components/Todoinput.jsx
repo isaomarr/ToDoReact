4502f2498cc640cafe76c35a3aeac448be8616ca
isaomarr <isomarov@outlook.com>
2026-07-08 17:52:54 +0400

    fix

--- src/Components/Todoinput.jsx
+++ src/Components/Todoinput.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import{PlusOutlined} from '@ant-design/icons'
+import React, { useState } from 'react'
+import { PlusOutlined } from '@ant-design/icons'
 import { DeleteOutlined } from '@ant-design/icons';
-import {Button, Input } from 'antd';
+import { Button, Input } from 'antd';
+
 
 const Todoinput = () => {
+  const [inputValue, setInputValue] = useState('')
+  const addTODO = () => {
+
+  }
   return (
     <div className='flex items-center gap-3'>
-      <Input onFocus={true}
-      style={{
-        width:400,
-      }} placeholder="Add Todo" />
-      <Button type="primary"><PlusOutlined />Add ToDo</Button>
+      <Input onChange={(e) => {
+        setInputValue(e.target.value)
+
+
+      }}
+        style={{
+          width: 400,
+        }} placeholder="Add Todo" />
+      <Button onClick={addTODO} type="primary"><PlusOutlined />Add ToDo</Button>
       <Button type='solid' color='danger' variant='solid'> <DeleteOutlined />Delete Al to do</Button>
     </div>
   )
